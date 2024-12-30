5692119ea4d055d53aa66f61e2914a4ea95a4b0d
Joel Cutler <joelcutler108@gmail.com>
2024-12-30 14:02:28 +0000

    Add boilerplate functions and struct defintions

--- cVec/main.c
+++ cVec/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+	int data;
+	Node* next;
+} Node;
+
+typedef struct {
+	Node* start;
+	size_t size;
+} Vector;
+
+void push(Vector vec, int value)
+{
+
+}
+
+void insert(Vector vec, int value, size_t position)
+{
+
+}
+
+void remove(Vector vec, int value)
+{
+
+}
+
+void remove_at(Vector vec, size_t position)
+{
+
+}
+
 int main()
 {
 	printf("Hello world!");
