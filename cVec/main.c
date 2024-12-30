3fc6d72f08afca4b69451b6b82d559ae04a76423
Joel Cutler <joelcutler108@gmail.com>
2024-12-30 16:29:02 +0000

    added push function

--- cVec/main.c
+++ cVec/main.c
@@ -3,7 +3,7 @@
 
 typedef struct {
 	int data;
-	Node* next;
+	struct Node* next;
 } Node;
 
 typedef struct {
@@ -11,29 +11,57 @@ typedef struct {
 	size_t size;
 } Vector;
 
-void push(Vector vec, int value)
+Node* _vec_create_node(int data)
+{
+	Node* node = malloc(sizeof(Node));
+	node->data = data;
+	node->next = NULL;
+	return node;
+}
+
+void vec_push(Vector* vec, int value)
+{
+	Node* newNode = _vec_create_node(value);
+
+	if (vec->size == 0)
+	{
+		vec->start = newNode;
+	}
+	else
+	{
+		Node* iNode = vec->start;
+		while (iNode->next != NULL)
+		{
+			iNode = iNode->next;
+		}
+		iNode->next = newNode;
+	}
+
+	vec->size++;
+}
+
+void vec_insert(Vector vec, int value, size_t position)
 {
 
 }
 
-void insert(Vector vec, int value, size_t position)
+void vec_remove(Vector vec, int value)
 {
 
 }
 
-void remove(Vector vec, int value)
-{
-
-}
-
-void remove_at(Vector vec, size_t position)
+void vec_remove_at(Vector vec, size_t position)
 {
 
 }
 
 int main()
 {
-	printf("Hello world!");
+	Vector vec = { NULL, 0 };
+
+	vec_push(&vec, 20);
+	vec_push(&vec, 22);
+	vec_push(&vec, 24);
 
 	return 0;
 }
