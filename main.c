5a76c5c3e0f154258d1878621cd2fe122456df13
Joel Cutler <joelcutler108@gmail.com>
2024-12-31 01:41:40 +0000

    updated launch and tasks for new setup

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
+#include "list/list.h"
 
 int main()
 {
-	List lst = lst_create();
+  List lst = lst_create();
 
-	lst_push_back(&lst, 2);
-	lst_push_back(&lst, 4);
-	lst_push_front(&lst, 1);
-	lst_insert(&lst, 3, 2);
+  lst_push_back(&lst, 2);
+  lst_push_back(&lst, 4);
+  lst_push_front(&lst, 1);
+  lst_insert(&lst, 3, 2);
 
-	lst_print(lst);
-	printf("\n");
+  lst_print(lst);
+  printf("\n");
 
-	lst_swap(&lst, 2, 1);
-	lst_print(lst);
+  lst_swap(&lst, 2, 1);
+  lst_print(lst);
 
-	return 0;
+  return 0;
 }
