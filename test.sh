9ff05b37f5ad195d3bce094aa6ead49374b5bd6e
Joel Cutler <joelcutler108@gmail.com>
2025-01-02 22:52:45 +0000

    run valgrind on tests

--- test.sh
+++ test.sh
@@ -9,8 +9,9 @@ set -e
 export GCC_ARGS="-lcunit -fprofile-arcs -ftest-coverage"
 ./build.sh $1
 
+# Run valgrind to test for memory leaks 
 # Run the tests reporting to an xml file
-./build/test_list.out build/test-list
+valgrind --leak-check=full ./build/test_list.out build/test-list
 # Prettify the test results
 mkdir -p "build/test-reports"
 xsltproc cunit-report.xsl build/test-list-Results.xml > build/test-reports/test-list-report.html
