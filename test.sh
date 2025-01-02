69e0a2fdcbadbd3b9c5fcf957b782a506fd95fd6
Joel Cutler <joelcutler108@gmail.com>
2025-01-02 00:40:06 +0000

    automatically create html report from tests

--- test.sh
+++ test.sh
@@ -3,13 +3,15 @@
 export GCC_ARGS="-lcunit -fprofile-arcs -ftest-coverage"
 ./build.sh $1
 
-./build/test_list.out
+./build/test_list.out build/test_list
 
-xsltproc cunit-to-html.xsl build/test_results.xml-Results.xml > build/test_results-Results.html
+mkdir -p "build/test-reports"
+xsltproc cunit-to-html.xsl build/test_list-Results.xml > build/test-reports/test_list-Results.html
 
 cd build
 gcov *.gcno
 cd ..
 
 lcov --capture --directory build --output-file build/coverage.info
+mkdir -p "build/coverage-report"
 genhtml build/coverage.info --output-directory build/coverage-report
