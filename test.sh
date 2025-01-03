202c0fb2e7979b1a94ffecd9aeb30d989604af0f
Joel Cutler <joelcutler108@gmail.com>
2025-01-03 23:25:34 +0000

    execute specific test if parameter passed

--- test.sh
+++ test.sh
@@ -12,23 +12,28 @@ mkdir -p "$OUTPUT_DIR/test-reports"
 mkdir -p "$OUTPUT_DIR/valgrind-reports"
 mkdir -p "$OUTPUT_DIR/coverage-report"
 
-# todo if $1 then only run on that test
+# Run specific test if argument is passed
+if [[ -n "$1" ]]; then
+    files=("tests/$1.c")
+else
+    files=($(find tests/ -name "*.c"))
+fi
 
 # Specify GCC args for testing and coverage
 export GCC_ARGS="-lcunit -fprofile-arcs -ftest-coverage"
 
 # Build and run all tests
-for file in $(find tests/ -name "*.c"); do
+for file in "${files[@]}"; do
     test_name=$(basename $file .c)
 
     # Build specified test file
     ./build.sh $file
-    complied_test="$OUTPUT_DIR/${test_name}.out"
+    compiled_test="$OUTPUT_DIR/${test_name}.out"
 
     # Run valgrind to test for memory leaks 
     # Run the tests reporting to an xml file
     valgrind_report="$OUTPUT_DIR/${test_name}-valgrind-report.xml"
-    valgrind --leak-check=full --xml=yes --xml-file=$valgrind_report $complied_test "$OUTPUT_DIR/$test_name"
+    valgrind --leak-check=full --xml=yes --xml-file=$valgrind_report $compiled_test "$OUTPUT_DIR/$test_name"
     test_report="$OUTPUT_DIR/${test_name}-Results.xml"
     # Prettify the test results
     xsltproc cunit-report.xsl $test_report > "$OUTPUT_DIR/test-reports/$test_name-report.html"
