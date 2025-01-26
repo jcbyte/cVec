02c140bfe1b50bd71c83babcdf9135d95a42a16a
Joel Cutler <joelcutler108@gmail.com>
2025-01-26 18:22:50 +0000

    created example script

--- examples/example_list.c
+++ examples/example_list.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "../include/list.h"
 #include <errno.h>
 
 int main()
 {
-  // todo create example list
-  List l = lst_create((int[]) {1, 2, 3}, 3);
+  // create a list with pre-initialised values
+  List l = lst_create((int[]) {10, 20, 40}, 3);
 
-  lst_insert(&l, 4, 10);
+  // add elements to the list
+  lst_push_back(&l, 50);
+  lst_insert(&l, 30, 2);
+
+  // Get elements at specific positions of list
+  for (int i = 0; i < lst_size(l); i++)
+  {
+    printf("Element at position %d: %d\n", i, lst_at(l, i));
+  }
+  printf("\n");
+
+  // Print the list for debugging
   lst_print(l);
+  printf("\n\n");
+
+  // Remove elements by value
+  if (lst_remove(&l, 30))
+  {
+    printf("Removed element 30\n");
+  }
+
+  lst_print(l);
+  printf("\n\n");
+
+  // Remove element by index
+  int value = lst_remove_at(&l, 420);
+  // Error handling
+  if (errno)
+  {
+    printf("Error removing element at 420\n\n");
+  }
+
+  // Swap elemets in list
+  lst_swap(&l, 0, 2);
+  lst_print(l);
+  printf("\n\n");
   
   lst_destroy(&l);
   return 0;
