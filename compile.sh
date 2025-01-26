38f37dfee5f8a1613eaec40be767569442c2ee66
Joel Cutler <joelcutler108@gmail.com>
2025-01-26 17:09:04 +0000

    better error handling on compile script

--- compile.sh
+++ compile.sh
@@ -12,18 +12,30 @@ else
     files=($(find src/ -name "*.c"))
 fi
 
+# Compile all files
 for file in "${files[@]}"; do
     lib_name=$(basename $file .c)
+
+    # Compile object file
     gcc -c $file -o "${OUTPUT_DIR}/${lib_name}.o"
+    if [ $? -ne 0 ]; then
+        echo -e "\033[1;31mError compiling\033[0m \033[36m${file}\033[0m\n"
+        exit 1
+    fi
+    # Create static library
     ar rcs "${OUTPUT_DIR}/lib${lib_name}.a" "${OUTPUT_DIR}/${lib_name}.o"
+     if [ $? -ne 0 ]; then
+        echo -e "\033[1;31mError creating static library for\033[0m \033[36m${file}\033[0m\n"
+        exit 1
+    fi
     rm ${OUTPUT_DIR}/${lib_name}.o
 
+    # Create shared library
     gcc -shared -o "${OUTPUT_DIR}/lib${lib_name}.so" $file
+    if [ $? -ne 0 ]; then
+        echo -e "\033[1;31mError creating shared library for\033[0m \033[36m${file}\033[0m\n"
+        exit 1
+    fi
 done
 
-if [ $? -eq 0 ]; then
-  echo -e "\n\033[1;Compiling successful.\033[0m - \033[36m${OUTPUT_DIR}\033[0m\n"
-else
-  echo -e "\n\033[1;31mCompiling unsuccessful.\033[0m\n"
-  exit 1
-fi
+echo -e "\n\033[1;32mCompiling successful.\033[0m - \033[36m${OUTPUT_DIR}\033[0m\n"
